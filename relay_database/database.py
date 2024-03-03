946c4f0fa2f997b5f4af5f91f793623c129ef825
Nana Kwame Yiadom Boachie <yiadom_deb@yiadom.net>
2024-03-03 18:23:20 +0000

    test db

--- relay_database/database.py
+++ relay_database/database.py
@@ -1,2 +1,17 @@
 #create a database file
 import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="relayuser",
+  password="TAIL",
+  database="relaydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM customers")
+
+results = mycursor.fetchall()
+
+# work with results
