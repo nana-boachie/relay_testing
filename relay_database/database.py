50f02f33efdb2d0ce9e82839785f426b583de6c0
Nana Kwame Yiadom Boachie <yiadom_deb@yiadom.net>
2024-03-03 18:04:04 +0000

    database please help

--- relay_database/database.py
+++ relay_database/database.py
@@ -1,10 +1,2 @@
+#create a database file
 import mysql.connector
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="yiadom",
-    password="12345678"
-    )
-
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE relay_databasev1")
